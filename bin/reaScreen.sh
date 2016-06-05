1d0923e82532a6664c5cdb0ddf4f3d631e4ef3bf
root <root@ip-172-31-29-30.us-west-2.compute.internal>
2016-06-05 11:37:17 -0400

    Added a new script for the screen

--- bin/reaScreen.sh
+++ bin/reaScreen.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+#
+# We print this script on screen to keep users updated
+
+LOG_ROUTE='/usr/local/rea_richmond/log/info.log'
 
 echo -e "\n\e[35mNotice: \e[1;32mRea Richmond - Preinterview test!\e[0m\n"
 sleep 5
-tail -f /usr/local/rea_richmond/log/info.log
+tail -f $LOG_ROUTE
